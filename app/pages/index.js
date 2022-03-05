05ad3eaaacb1ef62441998bba610b0aee282b82a
johnpagtama <pagtamajr@gmail.com>
2022-03-04 23:51:24 -0800

    Added header component

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,10 +1,12 @@
 import styles from '../styles/Home.module.css'
+import Header from '../components/header'
 import Slider from '../components/slider'
 
 const Home = () => {
 
   return (
     <>
+      <Header />
       <Slider category={`electronics`} />
       <Slider category={`women's clothing`} />
       <Slider category={`men's clothing`} />
