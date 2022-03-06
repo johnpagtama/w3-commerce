2d4b78dedaa71d11cec0a5a81953d514b3b20807
johnpagtama <pagtamajr@gmail.com>
2022-03-05 23:56:22 -0800

    Added button component

--- app/components/header.js
+++ app/components/header.js
@@ -1,14 +1,23 @@
 import styles from '../styles/Header.module.css'
 import { FcRating } from 'react-icons/fc'
+import { IoCart } from 'react-icons/io5'
+import Button from '../components/button'
 
 const Header = ({ }) => {
     return (
         <div className={`${styles.layout}`}>
-            <FcRating className={`${styles['layout__logo']}`} />
+            <FcRating className={`${styles['layout__logo']}`} size={50} />
+            <IoCart className={`${styles['layout__cart']}`} size={30} />
             <div className={`${styles['layout__auth']}`}>
-                <div className={`${styles['layout__auth__wallet']}`}>Wallet Sign in</div>
-                <div className={`${styles['layout__auth__signin']}`}>Email Sign in</div>
-                <div className={`${styles['layout__auth__signup']}`}>Email Sign up</div>
+                <div className={`${styles['layout__auth__wallet']}`}>
+                    <Button color='primary' text='Wallet Sign in' />
+                </div>
+                <div className={`${styles['layout__auth__signin']}`}>
+                    <Button color='secondary' text='Email Sign in' />
+                </div>
+                <div className={`${styles['layout__auth__signup']}`}>
+                    <Button color='secondary-outline' text='Email Sign up' />
+                </div>
             </div>
         </div>
     )
