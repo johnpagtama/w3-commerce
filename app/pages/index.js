a37534e19e15e3eb30918a7785bdebdccf938304
johnpagtama <pagtamajr@gmail.com>
2022-03-06 23:39:10 -0800

    Added side menu component

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,6 +3,8 @@ import Header from '../components/header'
 import Slider from '../components/slider'
 
 const Home = () => {
+  // Use later for popular items component
+  // [...new Set([...new Array(5).fill(null).map(a => a = Math.floor(Math.random() * 21))])]
 
   return (
     <>
