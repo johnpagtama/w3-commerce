2ba772d7b3c468b29652d5605634d7b6f21facf9
johnpagtama <pagtamajr@gmail.com>
2022-03-08 23:57:15 -0800

    Finished login form layout

--- app/components/button.js
+++ app/components/button.js
@@ -1,10 +1,10 @@
 import styles from '../styles/Button.module.css'
 import React from 'react'
 
-const Button = React.forwardRef(({ onClick, variant, color, type = 'button', text, link = '/' }, ref) => {
+const Button = React.forwardRef(({ onClick, variant, color, type = 'button', text, link = '/', width = '11rem' }, ref) => {
     if (variant !== 'link') {
         return (
-            <button ref={ref} onClick={onClick} className={`${styles['base']} ${styles[variant]} ${styles[color]}`} type={type}>{text}</button>
+            <button ref={ref} onClick={onClick} style={{ 'width': `${width}` }} className={`${styles['base']} ${styles[variant]} ${styles[color]}`} type={type}>{text}</button>
         )
     } else {
         return (
--- app/pages/login.js
+++ app/pages/login.js
@@ -6,10 +6,12 @@ const Login = () => {
         <div className={`${styles['layout']}`}>
             <form className={`${styles['layout__form']}`} action=''>
                 <div className={`${styles['layout__form__email']}`}>
-                    <input type='text' placeholder='Email' />
+                    <input className={`${styles['layout__form__email__input']}`} type='text' placeholder='Email' />
+                    <span className={`${styles['layout__form__email__focus__border']}`}></span>
                 </div>
                 <div className={`${styles['layout__form__password']}`}>
-                    <input type='text' placeholder='Password' />
+                    <input className={`${styles['layout__form__password__input']}`} type='text' placeholder='Password' />
+                    <span className={`${styles['layout__form__password__focus__border']}`}></span>
                 </div>
                 <div className={`${styles['layout__form__registration']}`}>
                     <p className={`${styles['layout__form__registration__link']}`}>
@@ -17,7 +19,7 @@ const Login = () => {
                     </p>
                 </div>
                 <div className={`${styles['layout__form__signin']}`}>
-                    <Button onClick={() => console.log('Logging in')} color='primary' text='Login' type='button' />
+                    <Button onClick={() => console.log('Logging in')} color='primary' text='Login' width='25rem' type='button' />
                 </div>
             </form>
         </div>
